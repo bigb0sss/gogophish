0bff321d508e387e032a7323a8520b2461cb4d31
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-09-27 21:49:09 -0400

    Update ec2_termination.sh

--- aws/ec2_termination.sh
+++ aws/ec2_termination.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/bash
+# Version         : 1.0
+# Created date    : 09/27/2020
+# Last update     : 09/27/2020
+# Author          : bigb0ss
+# Description     : AWC EC2 Instance Creation
+
 ec2=$(cat gophish_ec2.json | grep InstanceId | cut -d '"' -f 4)
 
 aws ec2 terminate-instances \
